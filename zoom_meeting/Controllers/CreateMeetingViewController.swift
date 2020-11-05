84ea1ab4e50150ef63135b04bf83c725dc8b52b2
Tommy <1999yuto@gmail.com>
2020-11-05 11:25:05 +0900

    ミーティング情報をJSONで書き出し

--- zoom_meeting/Controllers/CreateMeetingViewController.swift
+++ zoom_meeting/Controllers/CreateMeetingViewController.swift
@@ -8,15 +8,40 @@
 import UIKit
 import Eureka
 import Floaty
+import MobileRTC
+import MobileCoreServices
 
 class CreateMeetingViewController: FormViewController, FloatyDelegate {
     
+    struct MeetingInfo: Codable {
+        let title: String
+        let start: Int
+        let link: String
+        let agenda: [Agenda]
+    }
+    
+    struct Agenda: Codable {
+        let title: String
+        let duration: Int
+    }
+    
     var index = 1
+    var sharedAuthService:  MobileRTCAuthService?
+    var preMeetingService: MobileRTCPremeetingService?
+    var userInfo: MobileRTCAccountInfo?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        // Zoom SDK
+        sharedAuthService = MobileRTC.shared().getAuthService()
+        preMeetingService = MobileRTC.shared().getPreMeetingService()
+        preMeetingService?.delegate = self
+        
+        if sharedAuthService?.isLoggedIn() ?? false {
+            userInfo = sharedAuthService?.getAccountInfo()
+        }
         // Form
         form
             +++ Section("ミーティング情報")
@@ -47,6 +72,56 @@ class CreateMeetingViewController: FormViewController, FloatyDelegate {
         self.view.addSubview(floaty)
     }
     
+    @IBAction func create() {
+        var totalTime = 0
+        for i in 1...index {
+            let timeRow = form.rowBy(tag: "time\(i)") as! IntRow
+            totalTime += timeRow.value ?? 0
+        }
+        
+        guard let meetingService = preMeetingService,
+              let meeting = meetingService.createMeetingItem()
+        else { return }
+        let titleRow = form.rowBy(tag: "title") as! TextRow
+        let startingDateRow = form.rowBy(tag: "date") as! DateTimeInlineRow
+        let beforeHostRow = form.rowBy(tag: "beforeHost") as! SwitchRow
+        meeting.setMeetingTopic(titleRow.value ?? "Untitled")
+        meeting.setStartTime(startingDateRow.value ?? Date())
+        meeting.setDurationInMinutes(UInt(TimeInterval(totalTime)))
+        meeting.setAllowJoinBeforeHost(beforeHostRow.value ?? false)
+
+        meetingService.scheduleMeeting(meeting, withScheduleFor: userInfo?.getEmailAddress())
+        meetingService.destroy(meeting)
+    }
+    
+    func createMeetingInfo(title: String, startingDate: Date, link: String) {
+        var agendas: [Agenda] = []
+        
+        for i in 1...index {
+            let titleRow = form.rowBy(tag: "title\(i)") as! TextRow
+            let title = titleRow.value ?? "Untitled"
+            let timeRow = form.rowBy(tag: "time\(i)") as! IntRow
+            let time = timeRow.value ?? 0
+            agendas.append(Agenda(title: title, duration: time))
+        }
+        
+        let timeInUnix = startingDate.timeIntervalSince1970
+        
+        let meetingInfo = MeetingInfo(title: title,
+                                      start: Int(timeInUnix),
+                                      link: link,
+                                      agenda: agendas)
+        do {
+            let encoder = JSONEncoder()
+//            encoder.outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
+            let jsonData = try encoder.encode(meetingInfo)
+            let jsonString = String(data: jsonData, encoding: .utf8)
+            print(jsonString)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     func emptyFloatySelected(_ floaty: Floaty) {
         index += 1
         let titleRow = TextRow("title\(index)") {
@@ -62,14 +137,35 @@ class CreateMeetingViewController: FormViewController, FloatyDelegate {
         section.append(timeRow)
         form.append(section)
     }
-    
-    @IBAction func create() {
-        for i in 1...index {
-            let titleRow = form.rowBy(tag: "title\(i)") as! TextRow
-            let title = titleRow.value!
-            let timeRow = form.rowBy(tag: "time\(i)") as! IntRow
-            let time = timeRow.value!
-            print("\(title): \(time)")
+}
+
+extension CreateMeetingViewController: MobileRTCPremeetingDelegate {
+    func sinkSchedultMeeting(_ result: PreMeetingError, meetingUniquedID uniquedID: UInt64) {
+        guard result.rawValue == 0 else {
+            print("Zoom (User): Schedule meeting task failed, error code: \(result)")
+            return
         }
+        
+        print("Zoom (User): Schedule meeting task completed.")
+        let meeting = preMeetingService?.getMeetingItem(byUniquedID: uniquedID)
+        guard let id = meeting?.getMeetingNumber(),
+              let pass = meeting?.getMeetingPassword() else { return }
+        
+        createMeetingInfo(
+            title: meeting?.getMeetingTopic() ?? "Untitled",
+            startingDate: meeting?.getStartTime() ?? Date(),
+            link: "zoommtg://zoom.us/join?confno=\(id)&pwd=\(pass)")
+    }
+    
+    func sinkEditMeeting(_ result: PreMeetingError, meetingUniquedID uniquedID: UInt64) {
+        print(result)
+    }
+    
+    func sinkDeleteMeeting(_ result: PreMeetingError) {
+        print(result)
+    }
+    
+    func sinkListMeeting(_ result: PreMeetingError, withMeetingItems array: [Any]) {
+        print(result)
     }
 }
--- zoom_meeting/Controllers/MainViewController.swift
+++ zoom_meeting/Controllers/MainViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 import MobileRTC
 import MobileCoreServices
 
-class MainViewController: UIViewController, MobileRTCPremeetingDelegate {
+class MainViewController: UIViewController {
     
     var sharedAuthService:  MobileRTCAuthService?
-    var preMeetingService: MobileRTCPremeetingService?
     var userInfo: MobileRTCAccountInfo?
     
     @IBOutlet var userNameLabel: UILabel!
@@ -21,9 +20,6 @@ class MainViewController: UIViewController, MobileRTCPremeetingDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         sharedAuthService = MobileRTC.shared().getAuthService()
-        preMeetingService = MobileRTC.shared().getPreMeetingService()
-        
-        preMeetingService?.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,46 +30,8 @@ class MainViewController: UIViewController, MobileRTCPremeetingDelegate {
         }
     }
     
-    @IBAction func createMeeting() {
-//        guard let meetingService = preMeetingService,
-//              let meeting = meetingService.createMeetingItem()
-//        else { return }
-//        meeting.setMeetingTopic("Test")
-//        meeting.setStartTime(Date())
-//        meeting.setDurationInMinutes(UInt(TimeInterval(60)))
-//
-//        meetingService.scheduleMeeting(meeting, withScheduleFor: userInfo?.getEmailAddress())
-//        meetingService.destroy(meeting)
-    }
-    
     @IBAction func logout() {
         sharedAuthService?.logoutRTC()
         self.dismiss(animated: true, completion: nil)
-    }
-    
-    func sinkSchedultMeeting(_ result: PreMeetingError, meetingUniquedID uniquedID: UInt64) {
-        guard result.rawValue == 0 else {
-            print("Zoom (User): Schedule meeting task failed, error code: \(result)")
-            return
-        }
-        
-        print("Zoom (User): Schedule meeting task completed.")
-        let meeting = preMeetingService?.getMeetingItem(byUniquedID: uniquedID)
-        let id = meeting?.getMeetingNumber() ?? 0
-        let pass = meeting?.getMeetingPassword() ?? ""
-        
-        print("zoommtg://zoom.us/join?confno=\(id)&pwd=\(pass)")
-    }
-    
-    func sinkEditMeeting(_ result: PreMeetingError, meetingUniquedID uniquedID: UInt64) {
-        print(result)
-    }
-    
-    func sinkDeleteMeeting(_ result: PreMeetingError) {
-        print(result)
-    }
-    
-    func sinkListMeeting(_ result: PreMeetingError, withMeetingItems array: [Any]) {
-        print(result)
     }
 }
